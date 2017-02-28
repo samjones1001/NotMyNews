921db39dc20a7da34653436ab946dfcb5a26984e
Sam Jones <sam.jones494@googlemail.com>
2017-02-28 16:07:42 +0000

    API call WIP

--- notmynews/app/assets/javascripts/stories.js
+++ notmynews/app/assets/javascripts/stories.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-	document.getElementById("1").innerHTML = "<ul><li>Super</li></ul>";
+
+	function getHeadline() {
+		var results = ''
+		var papers = ['buzzfeed', 'the-guardian-uk', 'the-huffington-post', 'the-new-york-times', 'mirror']
+
+		for (var i = 0; i < papers.length; i++) {
+			$.get("https://newsapi.org/v1/articles?source=" + papers[i] + "&apiKey=a6b03a1b52f64218a40f82bd352dd8f9", function(data){
+				for (var i = 0; i< 3; i++) {
+					results += (data.articles[i].title) + ' ';
+					console.log('hi!')
+				}
+
+			});
+			// $elem = document.getElementById('storylist')
+
+		}
+
+	}
+	getHeadline()
+
 });
--- notmynews/app/controllers/stories_controller.rb
+++ notmynews/app/controllers/stories_controller.rb
@@ -1,7 +1,20 @@
 class StoriesController < ApplicationController
 	#before[authencate user ]
 	def index
-
+		#@paper_list = ['buzzfeed', 'the-guardian-uk', 'the-huffington-post', 'the-new-york-times', 'mirror']
 	end
 
 end
+
+
+
+
+#
+# id | password | email | choice
+# 1	 | password | test  | left
+#
+#
+#
+# id | name 			| position
+# 1	 | guardian		| left
+# 2	 | daily mail | right
