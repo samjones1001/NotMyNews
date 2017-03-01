746e46a47783cd64006dd9a3d19eda173f698339
Joe Marriott <marriott90@gmail.com>
2017-03-01 17:26:59 +0000

    rake db:setup modify schema

--- notmynews/db/schema.rb
+++ notmynews/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301111036) do
+ActiveRecord::Schema.define(version: 20170301164054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "papers", force: :cascade do |t|
+    t.string "title"
+    t.string "position"
+  end
 
   create_table "student", id: :integer, force: :cascade do |t|
     t.text "name", null: false
