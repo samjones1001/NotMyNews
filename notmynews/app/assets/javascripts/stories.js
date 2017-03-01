07dfe8169972612642d30bf3c72f2e7d11188083
Basile Koko <basile_koko@hotmail.com>
2017-03-01 10:38:37 +0000

    add source to page

--- notmynews/app/assets/javascripts/stories.js
+++ notmynews/app/assets/javascripts/stories.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
     var apiKey = '52fe927b3c4f4fe28bcf38d0b2e82875'
     var url = 'https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + apiKey
     $.get(url , function(data) {
-      console.log(data.articles)
-      $('#storylist').append("<ul><li>" + "<a href='" + data.articles[0].url + "'>" + data.articles[0].title + "</a>" + "<br>" + data.articles[0].description + "</li></ul>" + "<br>" + "<img src='"+ data.articles[0].urlToImage + "'>")
+      var paperName = source.replace(/-/g, " ").toUpperCase()
+      console.log(paperName)
+      $('#storylist').append("<ul><li>" + "<a href='" + data.articles[0].url + "'>"  + data.articles[0].title + "</a>" + "<br>" + data.articles[0].description + "<br>" + paperName + "<br>" + "<img src='"+ data.articles[0].urlToImage + "'>" + "</li></ul>" + "<br>")
 
     })
   }
