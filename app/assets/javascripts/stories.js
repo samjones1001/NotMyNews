795929f76ee78d985eda64308984b044c29fb426
Basile Koko <basile_koko@hotmail.com>
2017-03-02 16:22:47 +0000

    add save button to each user story

--- app/assets/javascripts/stories.js
+++ app/assets/javascripts/stories.js
@@ -6,7 +6,19 @@ $(document).ready(function(){
     var url = 'https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + apiKey;
     $.get(url , function(data) {
       var paperName = source.replace(/-/g, " ").toUpperCase();
-      $('#storylist').append("<ul><li>" + "<a href='" + data.articles[articleNo].url + "'>"  + data.articles[articleNo].title + "</a>" + "<br>" + data.articles[articleNo].description  + "<br>" + paperName + "<br><img src='" + data.articles[articleNo].urlToImage + "'height='150' ><br></li></ul><br>" );
+      $('#storylist').append("<ul><li>" + "<a href='" + data.articles[articleNo].url + "'>"
+         + data.articles[articleNo].title + "</a>" + "<br>" + data.articles[articleNo].description
+         + "<br>" + paperName + "<br><img src='" + data.articles[articleNo].urlToImage
+         + "' height='150'><br></li></ul><br>"
+         + "<form action='/savedlinks' method='post'>"
+         + "<input class='input' type='text' style='display:none;' name='Title' value='"  + data.articles[articleNo].title + "'>"
+         + "<input class='input' type='text' style='display:none;' name='Url' value='" + data.articles[articleNo].url + "'>"
+         + "<input class='input' type='text' style='display:none;' name='Description' value='" + data.articles[articleNo].description +"'>"
+         + "<input class='input' type='text'  style='display:none;' name='Source' value='" + paperName + "'>"
+         + "<input class='input' type='text' style='display:none;' name='Image' value='" + data.articles[articleNo].urlToImage + "'>"
+         + "<input type='submit' value='Save'>" +
+           "</form>"
+      );
     });
   }
 
