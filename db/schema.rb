2bf1179ec613f65cbcb236bda5a860cdc4249b72
Justinio14 <jbutwell@hotmail.com>
2017-03-03 10:38:29 +0000

    added saved links and stories button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302112915) do
+ActiveRecord::Schema.define(version: 20170303103348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170302112915) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.string   "source"
     t.index ["user_id"], name: "index_saved_links_on_user_id", using: :btree
   end
 
