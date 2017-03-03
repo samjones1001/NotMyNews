e795bb41235abe9587d7790d75a4da2c7efbb68b
Sam Jones <sam.jones494@googlemail.com>
2017-03-03 10:27:43 +0000

    heroku fix attempt 2

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-  // for(var i = 0; i < 5; i++) {
-  //   document.getElementById('splashscreen').innerHTML += '<p>HI!</p>'
-  // }
-
-  function newsArticles(source, articleNo) {
-
-    var apiKey = '52fe927b3c4f4fe28bcf38d0b2e82875';
-    var url = 'https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + apiKey;
-    $.get(url , function(data) {
-      var paperName = source.replace(/-/g, " ").toUpperCase();
-      document.getElementById('splashscreen').innerHTML += ("<ul class='storylist'><li><div class='splashbox'><div class='storyinfo'><a href='" + data.articles[articleNo].url + "'>"  + data.articles[articleNo].title + "</a><br>" + data.articles[articleNo].description  + "<br>" + paperName + "<br></div><img class='storyphoto' src='" + data.articles[articleNo].urlToImage + "'></div></li><hr class='storysplitter'></ul>" );
-    });
-  }
 
   $.get('/papers', function(data){
     var papers = data.paper;
-
-
      for (var i = 0; i < 3; i++){
-       newsArticles(papers[Math.floor((Math.random() * papers.length))], 0);
+       newsArticles(papers[Math.floor((Math.random() * papers.length))], Math.floor((Math.random() * 3)));
       }
-
   });
 });
+
+function newsArticles(source, articleNo) {
+
+  var apiKey = '52fe927b3c4f4fe28bcf38d0b2e82875';
+  var url = 'https://newsapi.org/v1/articles?source=' + source + '&apiKey=' + apiKey;
+  $.get(url , function(data) {
+    var paperName = source.replace(/-/g, " ").toUpperCase();
+    document.getElementById('splashstorylist').innerHTML += ("<li style='list-style:none'>" +
+                                                              "<div class='splashbox'><div class='storyinfo'>" +
+                                                              "<a href='" + data.articles[articleNo].url + "'>"  +
+                                                              data.articles[articleNo].title + "</a><br>" +
+                                                              data.articles[articleNo].description  + "<br>" +
+                                                              paperName + "<br></div><img class='storyphoto' src='" +
+                                                              data.articles[articleNo].urlToImage + "'></div></li><hr class='storysplitter'>");
+  });
+}
